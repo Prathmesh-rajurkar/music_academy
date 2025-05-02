b13ed4f52c236b119b83c7e847511c9e8ac80746
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-05-02 20:07:22 +0530

    moving button

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,11 +1,17 @@
 "use client";
 import Link from "next/link";
 import React from "react";
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
 
 function HeroSection() {
   return (
     <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
       <div className="p-4 relative z-10 w-full text-center">
+        <Spotlight
+          className="-top-40 left-0 md:left-100 md:-top-30"
+          fill="white"
+        />
         <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">
           Master The Art Of Music
         </h1>
@@ -15,7 +21,11 @@ function HeroSection() {
           skills, join us to unlock your true potential.
         </p>
         <div className="mt-4">
-          <Link href={"/courses"}>Explore Courses</Link>
+          <Link href={"/courses"}>
+            <Button className="bg-black" borderRadius="1.75rem" duration={2000}>
+              Explore Courses
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
