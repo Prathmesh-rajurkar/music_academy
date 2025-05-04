954cbeedf7dd2215273e708fdfe23a850f2613da
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-05-04 06:23:52 +0530

    spw dome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructor from "@/components/Instructor";
+import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebniars from "@/components/UpcomingWebniars";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import Image from "next/image";
 
@@ -8,8 +11,11 @@ export default function Home() {
     <div>
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection />
-        <FeaturedCourses/>
-        <WhyChooseUs/>
+        <FeaturedCourses />
+        <WhyChooseUs />
+        <TestimonialCards />
+        <UpcomingWebniars />
+        <Instructor />
       </main>
     </div>
   );
