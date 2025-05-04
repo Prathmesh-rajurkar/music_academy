8544891dbef1cd01f4c63a3df32972c4f42f6d88
Prathmesh-rajurkar <prathmeshrajurkar199@gmail.com>
2025-05-04 13:18:50 +0530

    complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
 import Instructor from "@/components/Instructor";
 import TestimonialCards from "@/components/TestimonialCards";
@@ -16,6 +17,7 @@ export default function Home() {
         <TestimonialCards />
         <UpcomingWebniars />
         <Instructor />
+        <Footer/>
       </main>
     </div>
   );
